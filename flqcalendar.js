dd52c2ffdaa1b8e45b9bfe3d741ab4d475853bb7
Christian <cbiggins@localhost.localdomain>
2009-02-17 19:16:05 +1100

    Fixed positioning bug, date callback bug, added year selection arrows

--- flqcalendar.js
+++ flqcalendar.js
@@ -1,9 +1,29 @@
-
     /**
-     * $Id: holcalendar.js 146 2008-11-09 23:55:36Z mlittle $
-     */
+    * Copyright 2009 Michael Little, Christian Biggins
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program. If not, see <http://www.gnu.org/licenses/>.
+    */
+    
+    /**
+    * FLQ.Calendar - Fliquid Calendar popup for form selection.
+    * Currently dependant on The Yahoo YUI Event and Dom libs. Planning on changing this soon
+    *
+    * Version: 1.0.1 BETA
+    * Last Modified: 17/02/2009
+    */
 
-    HOL.Calendar = function(dateformat) {
+    FLQ.Calendar = function(dateformat, showyear) {
         this.date = new Date();
         this.widgetId;
         this.d = YAHOO.util.Dom;
@@ -16,11 +36,13 @@
         this.target;
         this.location;
         this.dateformat = dateformat;
+        if (arguments.length < 2) showyear = false;
+        this.showyear = showyear;
     }
 
-    HOL.Calendar.prototype = new Object();
+    FLQ.Calendar.prototype = new Object();
 
-    HOL.Calendar.prototype.Build = function(e, id) {
+    FLQ.Calendar.prototype.Build = function(e, id) {
         this.DestroyDiv();
         if (e) {
             this.target = this.e.getTarget(e);
@@ -34,39 +56,52 @@
         this.CreateDiv(location);
     }
 
-    HOL.Calendar.callBack = function(val) { }
+    FLQ.Calendar.callBack = function(val) { }
 
-    HOL.Calendar.prototype.updateTime = function() {
+    FLQ.Calendar.prototype.updateTime = function() {
         var tempdate = new Date();
-        this.date.setTime(tempdate.getTime());
+        this.date.setHours(tempdate.getHours());
+        this.date.setMinutes(tempdate.getMinutes());
     }
 
-    HOL.Calendar.prototype.nextMonth = function() {
+    FLQ.Calendar.prototype.nextMonth = function() {
         this.DestroyDiv();
         this.date.setMonth(parseInt(this.date.getMonth()) + 1);
         this.Build();
     }
 
-    HOL.Calendar.prototype.prevMonth = function() {
+    FLQ.Calendar.prototype.prevMonth = function() {
         this.DestroyDiv();
         this.date.setMonth(parseInt(this.date.getMonth()) - 1);
         this.Build();
     }
 
-    HOL.Calendar.prototype.ClickDay = function(e) {
+    FLQ.Calendar.prototype.prevYear = function() {
+        this.DestroyDiv();
+        this.date.setFullYear(this.date.getFullYear() - 1);
+        this.Build();
+    }
+
+    FLQ.Calendar.prototype.nextYear = function() {
+        this.DestroyDiv();
+        this.date.setFullYear(this.date.getFullYear() + 1);
+        this.Build();
+    }
+
+    FLQ.Calendar.prototype.ClickDay = function(e) {
         this.updateTime();
         var DayTarget = this.e.getTarget(e);
         this.date.setDate(parseInt(DayTarget.firstChild.nodeValue));
-        HOL.Calendar.callBack(this.date.strftime(this.dateformat));
+        FLQ.Calendar.callBack(this.date.strftime(this.dateformat));
         this.DestroyDiv();
     }
 
-    HOL.Calendar.prototype.DestroyDiv = function() {
+    FLQ.Calendar.prototype.DestroyDiv = function() {
         var div = document.getElementById('HolCal_Div');
         if (div) div.parentNode.removeChild(div);
     }
 
-    HOL.Calendar.prototype.CreateDiv = function() {
+    FLQ.Calendar.prototype.CreateDiv = function() {
         var body = document.body;
         var divcont = document.createElement('div');
 
@@ -75,6 +110,23 @@
         this.buildContents(divcont);
 
         body.appendChild(divcont);
+
+        var divWidth = 130;
+        if (this.showyear) {
+            var divHeight = 170;
+        } else {
+            var divHeight = 150;
+        }
+
+        // If the popup div is positioned outside the current window, we need to put it back in the window. Don't want any pesky scrollbars!
+        if (this.location[1] + divHeight > window.innerHeight) {
+            var topOffset = window.innerHeight - divHeight;
+            this.location[1] = topOffset - 10; // Add an extra 10px just to be sure
+        }
+        if (this.location[0] + divWidth > window.innerWidth) {
+            var rightOffset = window.innerWidth - divWidth;
+            this.location[0] = rightOffset - 10;
+        }
 
         if (this.location) {
             this.d.setXY(divcont, this.location);
@@ -85,12 +137,19 @@
         var prevMonth = document.getElementById('HolCalPrevMonth');
         var closeCal =  document.getElementById('HolCloseCal');
 
+        if (this.showyear) {
+            var prevYear = document.getElementById('HolCalPrevYear');
+            var nextYear = document.getElementById('HolCalNextYear');
+            this.e.addListener(prevYear, 'click', function () { w.prevYear(); });
+            this.e.addListener(nextYear, 'click', function () { w.nextYear(); });
+        }
+
         this.e.addListener(nextMonth, 'click', function () { w.nextMonth(); });
         this.e.addListener(prevMonth, 'click', function () { w.prevMonth(); });
         this.e.addListener(closeCal, 'click', function () { w.DestroyDiv(); });
     }
 
-    HOL.Calendar.prototype.buildContents = function(divcont) {
+    FLQ.Calendar.prototype.buildContents = function(divcont) {
         divcont.appendChild(table = document.createElement('table'));
         this.d.addClass(table, 'HolCal_table');
         table.setAttribute('id', 'HolCal_Table');
@@ -130,6 +189,28 @@
         span.setAttribute('title', 'Close');
         span.appendChild(document.createTextNode('x'));
 
+        if (this.showyear) { // Need another row for the year display
+            table.appendChild(tr = document.createElement('tr'));
+            tr.appendChild(td = document.createElement('td'));
+            td.appendChild(span = document.createElement('span'));
+            span.appendChild(document.createTextNode('<'));
+            this.d.addClass(span, 'HolCal_clickable');
+            span.setAttribute('id', 'HolCalPrevYear');
+            span.setAttribute('alt', 'Previous Year');
+            span.setAttribute('title', 'Previous Year');
+            tr.appendChild(td = document.createElement('td'));
+            td.setAttribute('colspan', 4);
+            this.d.addClass(td, 'HolCal_monthname');
+            td.appendChild(document.createTextNode(this.date.getFullYear()));
+            tr.appendChild(td = document.createElement('td'));
+            td.appendChild(span = document.createElement('span'));
+            span.appendChild(document.createTextNode('>'));
+            this.d.addClass(span, 'HolCal_clickable');
+            span.setAttribute('id', 'HolCalNextYear');
+            span.setAttribute('alt', 'Next Year');
+            span.setAttribute('title', 'Next Year');
+        }
+
         table.appendChild(tr = document.createElement('tr'));
         for (i = 0; i < this.days_short.length; i++) {
             tr.appendChild(td = document.createElement('td'));
@@ -140,7 +221,7 @@
         this.buildDayRows(table);
     }
 
-    HOL.Calendar.prototype.buildDayRows = function(table) {
+    FLQ.Calendar.prototype.buildDayRows = function(table) {
         var days = '';
         var display_date = 1;
         var next_month_display_date = 1;
@@ -176,7 +257,7 @@
                         td.appendChild(span = document.createElement('span'));
                         this.d.addClass(td, 'HolCal_cell');
                         this.d.addClass(td, 'HolCal_clickable');
-                        span.setAttribute('id', 'HolCalDay' + display_date);
+                        span.setAttribute('id', 'HolCalDay' + display_date + this.date.getMonth() + this.date.getFullYear());
                         span.appendChild(document.createTextNode(display_date));
                         this.e.addListener(span, 'click', function(e) { t.ClickDay(e); });
                     } else {
@@ -192,7 +273,7 @@
         }
     }
 
-    HOL.Calendar.prototype.isLeapYear = function(year) {
+    FLQ.Calendar.prototype.isLeapYear = function(year) {
         year = parseInt(year);
         if (year%4 == 0) {
             if (year%100 != 0) {
@@ -208,7 +289,7 @@
         return false;
     }
 
-    HOL.Calendar.prototype.getStartDay = function() {
+    FLQ.Calendar.prototype.getStartDay = function() {
         var tempdate = this.date;
         tempdate.setDate(1);
         if (tempdate.getDay() == 0) { // A Sunday
